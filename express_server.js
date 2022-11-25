56df2fb9b381937c7f2f5238d72626bee7e5b426
Andrew Brown <andrew.robert.brown@gmail.com>
2022-11-25 00:01:44 +0000

    Add urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -3,10 +3,26 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomStrings() {
+  let tinyURL = "";
+
+  for (let i = 0; i < 6; i++) {
+    if (Math.random() > 0.5) {
+      tinyURL += Math.floor(Math.random() * 10);
+    } else {
+      let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+      tinyURL += alphabet.charAt(Math.floor(Math.random() * 52));
+    }
+  }
+  
+  return tinyURL;
 };
 
 app.get("/", (req, res) => {
@@ -14,9 +30,17 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  res.render("urls_index", {
-    urlDatabase: urlDatabase
-  });
+  const templateVars = {urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send(generateRandomStrings()); // Respond with tinyURL
 });
 
 app.get("/urls/:id", (req, res) => {
